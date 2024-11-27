b759a06dfa0a0b05a85b5a1d55f236349480f622
Gerhard Unger <gerhard.unger@tugraz.at>
2024-11-27 11:51:08 +0100

    Complement provider class

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -68,6 +68,16 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
     }
 
     /**
+     * Get table names of tables of the plugin which store personal data.
+     *
+     *
+     */
+    public static function get_tablenames_except_userlist() {
+        return ['local_oer_courseinfo', 'local_oer_coursetofile', 'local_oer_elements', 'local_oer_log',
+                 'local_oer_preference', 'local_oer_snapshot'];
+    }
+
+    /**
      * Delete all users from userlist.
      *
      * @param \context $context
@@ -75,13 +85,22 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
      * @throws \dml_exception
      */
     public static function delete_data_for_all_users_in_context(\context $context) {
-        if ($context->contextlevel != CONTEXT_SYSTEM) {
-            return;
-        }
         global $DB;
-        $users = $DB->get_records('local_oer_userlist');
-        foreach ($users as $user) {
-            static::delete_user_data($user->userid);
+
+        if ($context->contextlevel == CONTEXT_SYSTEM) {
+            $users = $DB->get_records('local_oer_userlist');
+            foreach ($users as $user) {
+                static::delete_user_data($user->userid);
+
+            }
+        }
+
+        if ($context->contextlevel == CONTEXT_COURSE) {
+            $tablenames = self::get_tablenames_except_userlist();
+            $params = ['courseid' => $context->instanceid];
+            foreach ($tablenames as $tablename) {
+                $DB->delete_records($tablename, $params);
+            }
         }
     }
 
@@ -93,15 +112,26 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
      * @throws \dml_exception
      */
     public static function delete_data_for_user(approved_contextlist $contextlist) {
+        global $DB;
         if (empty($contextlist->count())) {
             return;
         }
+
+        $tablenames = self::get_tablenames_except_userlist();
         $userid = $contextlist->get_user()->id;
         foreach ($contextlist->get_contexts() as $context) {
-            if ($context->contextlevel != CONTEXT_SYSTEM) {
-                continue;
+
+            if ($context->contextlevel == CONTEXT_SYSTEM) {
+                static::delete_user_data($userid);
             }
-            static::delete_user_data($userid);
+
+            if ($context->contextlevel == CONTEXT_COURSE) {
+                $params = ['courseid' => $context->instanceid, 'usermodified' => $userid];
+                foreach ($tablenames as $tablename) {
+                    $DB->delete_records($tablename, $params);
+                }
+            }
+
         }
     }
 
@@ -113,14 +143,26 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
      * @throws \dml_exception
      */
     public static function delete_data_for_users(approved_userlist $userlist) {
+        global $DB;
         $context = $userlist->get_context();
-        if ($context->contextlevel != CONTEXT_SYSTEM) {
-            return;
+
+        if ($context->contextlevel == CONTEXT_SYSTEM) {
+            $users = $userlist->get_userids();
+            foreach ($users as $userid) {
+                static::delete_user_data($userid);
+            }
         }
-        $users = $userlist->get_userids();
-        foreach ($users as $userid) {
-            static::delete_user_data($userid);
+
+        if ($context instanceof \context_course) {
+            $tablenames = self::get_tablenames_except_userlist();
+            [$usersql, $userparams] = $DB->get_in_or_equal($userlist->get_userids(), SQL_PARAMS_NAMED);
+            $select = "courseid = :courseid AND usermodified {$usersql}";
+            $params = ['courseid' => $context->instanceid] + $userparams;
+            foreach ($tablenames as $tablename) {
+                $DB->delete_records_select($tablename, $select, $params);
+            }
         }
+
     }
 
     /**
@@ -133,23 +175,70 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
      */
     public static function export_user_data(approved_contextlist $contextlist) {
         global $DB;
+        $tablenames = self::get_tablenames_except_userlist();
 
-        $sql = "SELECT * FROM {local_oer_userlist} ou WHERE ou.userid = :userid";
-        if ($userrecord = $DB->get_record_sql($sql, ['userid' => $contextlist->get_user()->id])) {
-            $data = (object) [
-                    'userid' => $userrecord->userid,
-                    'type' => $userrecord->type,
-                    'timecreated' => transform::datetime($userrecord->timecreated),
-            ];
-            writer::with_context(\context_system::instance())->export_data(
-                    [
-                            get_string('pluginname', 'local_oer'),
-                    ], $data);
+        if (in_array(1, $contextlist->get_contextids())) {
+            $sql = "SELECT * FROM {local_oer_userlist} ou WHERE ou.userid = :userid";
+            if ($userrecord = $DB->get_record_sql($sql, ['userid' => $contextlist->get_user()->id])) {
+                $data = (object) [
+                        'userid' => $userrecord->userid,
+                        'type' => $userrecord->type,
+                        'timecreated' => transform::datetime($userrecord->timecreated),
+                ];
+                writer::with_context(\context_system::instance())->export_data(
+                        ['local_oer_userlist'], $data);
+            }
+        }
+
+        $contexts = array_filter($contextlist->get_contexts(), function($context) {
+            if ($context->contextlevel == CONTEXT_COURSE) {
+                    return $context;
+            }
+        });
+
+        if (empty($contexts)) {
+            return;
+        }
+        $courseids = array_map(function($context) {
+            return $context->instanceid;
+        }, $contexts);
+
+        foreach ($tablenames as $table) {
+            
+            [$insql, $params] = $DB->get_in_or_equal($courseids, SQL_PARAMS_NAMED);
+            $sql = "SELECT s.*, c.id as courseid
+                      FROM {" . $table . "} s
+                      JOIN {course} c ON s.courseid = c.id
+                     WHERE s.usermodified = :usermodified AND c.id $insql
+                     ORDER BY c.id ASC";
+            $params['usermodified'] = $contextlist->get_user()->id;
+            $records = $DB->get_records_sql($sql, $params);
+
+            $statsrecords = [];
+            foreach ($records as $record) {
+                $context = \context_course::instance($record->courseid);
+                if (!isset($statsrecords[$record->courseid])) {
+                    $statsrecords[$record->courseid] = new \stdClass();
+                    $statsrecords[$record->courseid]->context = $context;
+                }
+                if (property_exists($record, "timecreated")) {
+                    $record->timecreated = transform::datetime($record->timecreated);
+                }
+                if (property_exists($record, "timemodified")) {
+                    $record->timemodified = transform::datetime($record->timemodified);
+                }
+                $statsrecords[$record->courseid]->entries[] = (array) $record;
+            }
+            foreach ($statsrecords as $coursestats) {
+                \core_privacy\local\request\writer::with_context($coursestats->context)->export_data([$table],
+                        (object) $coursestats->entries);
+            }
+
         }
     }
 
     /**
-     * Userlist is always in system context.
+     *
      *
      * @param int $userid
      * @return contextlist
@@ -158,10 +247,21 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
     public static function get_contexts_for_userid(int $userid): contextlist {
         global $DB;
         $contextlist = new contextlist();
-        if (!$DB->record_exists('local_oer_userlist', ['userid' => $userid])) {
-            return $contextlist;
+
+        $tablenames = self::get_tablenames_except_userlist();
+        foreach ($tablenames as $tablename) {
+            $params = ['userid' => $userid, 'contextcourse' => CONTEXT_COURSE];
+            $sql = "SELECT ctx.id
+                FROM {context} ctx
+                JOIN {" .$tablename ."} tabname ON tabname.courseid = ctx.instanceid AND tabname.usermodified = :userid
+                WHERE ctx.contextlevel = :contextcourse";
+            $contextlist->add_from_sql($sql, $params);
         }
-        $contextlist->add_system_context();
+
+        if ($DB->record_exists('local_oer_userlist', ['userid' => $userid])) {
+            $contextlist->add_system_context();
+        }
+
         return $contextlist;
     }
 
@@ -173,16 +273,24 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
     public static function get_users_in_context(userlist $userlist) {
         $context = $userlist->get_context();
 
-        if (!$context instanceof \context_system) {
-            return;
+        if ($context instanceof \context_system) {
+            $sql = "SELECT userid FROM {local_oer_userlist} ORDER BY userid ASC";
+            $userlist->add_from_sql('userid', $sql, []);
         }
 
-        $sql = "SELECT userid FROM {local_oer_userlist} ORDER BY userid ASC";
-        $userlist->add_from_sql('userid', $sql, []);
+        if ($context->contextlevel == CONTEXT_COURSE) {
+            $params = ['courseid' => $context->instanceid];
+            $tablenames = self::get_tablenames_except_userlist();
+            foreach ($tablenames as $tablename) {
+                $sql = "SELECT usermodified FROM {" .$tablename. "} WHERE courseid = :courseid";
+                $userlist->add_from_sql('usermodified', $sql, $params);
+            }
+        }
+
     }
 
     /**
-     * This does the deletion of user data in the userlist table.
+     * This does the deletion of user data.
      *
      * @param int $userid Moodle user id
      * @return void
